7d8fe950c725cc1a32893aad265fa87e2c11b870
AlexsandroMO <sandrobass@htomail.com>
2021-02-21 19:57:32 -0300

    start app

---
+++ static/js/scripts.js
@@ -0,0 +1,17 @@
+
+function clear_all(){
+
+    console.log('Entrou Clear_All')
+}
+
+
+
+function index(){
+    console.log('Entrou Index')
+}
+
+
+
+function singed(){
+    console.log('Entrou Singed')
+}
